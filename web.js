1de374012bbf48a072705f46e573f8fff29634c7
MyCraftServerMC <47946206+MyCraftServerMC@users.noreply.github.com>
2019-07-02 17:54:34 +0200

    fail2

---
+++ web.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const dotenv = require('dotenv').config();
+var port = process.env.PORT || 80;
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+http.listen(port, function(){
+  console.log('listening on port:'+port);
+});
+
+io.on('connection', function(socket){
+  console.log('connected');
+
+  socket.on('disconnect', function(){
+    console.log('disconnected');
+  });
