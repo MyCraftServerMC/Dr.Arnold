f574004a68322719553635feb946db59848f6633
MyCraftServerMC <47946206+MyCraftServerMC@users.noreply.github.com>
2019-09-07 13:03:44 +0200

    Web fix 5

--- index.js
+++ index.js
@@ -9,14 +9,6 @@ var io = require('socket.io')(http);
 const dotenv = require('dotenv').config();
 var port = process.env.PORT || 80;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/public/index.html');
-});
-
-http.listen(port, function(){
-  console.log('listening on port:'+port);
-});
-
 io.on('connection', function(socket){
   console.log('connected');
 
